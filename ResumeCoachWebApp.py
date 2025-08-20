aea956b27bbf7feea0d9ad0cca83792c6ba6c55f
Srinivasa Yanaparti <Srinivasa.yanaparti@gmail.com>
2025-08-19 17:50:00 -0700

    added my simple code

--- ResumeCoachWebApp.py
+++ ResumeCoachWebApp.py
@@ -1,15 +1,15 @@
 import streamlit as st
 
-st.title("My Simple Streamlit App")
-st.write("Hello, My name is srinivas")
+st.title("Resume Coach Web Application")
+st.write("Hello, My name is Srinivasa Yanaparti")
 
 def process_text(text):
     # Add your text processing logic here
     # For example, let's convert text to uppercase
     return text.upper()
 
-st.title("Text Processing App")
-text_input = st.text_area("Enter your text")
+#st.title("Text Processing App")
+text_input = st.text_area("Enter your Resume in text format")
 
 if st.button("Process Text"):
     processed_text = process_text(text_input)
